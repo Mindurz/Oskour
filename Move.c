7fb4c6f3debcd6646fcc7fc2e2448ca2f327bd8b
Mindurz <nicolas.minder@epfl.ch>
2020-04-02 11:04:19 +0200

    Clean le code

--- Move.c
+++ Move.c
@@ -5,33 +5,15 @@
 
 #include <sensors/proximity.h>
 #include <motors.h>
-//#include <sensors/VL53L0X/VL53L0X.h>
-//#include <leds.h>
-//#include <audio/play_melody.h>
-//#include <spi_comm.h>
-//#include <audio_processing.h>
+
 #include "Move.h"
 #include <main.h>
-//#include <process_image.h>
 
-#define VITESSE_NORMALE 600
-#define LEGERE_CORRECTION 30
 #define NB_PROX_SENSORS 8
-#define SAFETY_PROX_VALUE 200
-#define FRONT_RIGHT_IR_SENSOR 0
-#define DIAGONALE_RIGHT_IR_SENSOR 1
-#define RIGHT_IR_SENSOR 2
 #define LEFT_IR_SENSOR 5
-#define DIAGONALE_LEFT_IR_SENSOR 6
-#define FRONT_LEFT_IR_SENSOR 7
 #define QUARTER_TURN 90
 #define U_TURN 180
-#define MIN_TIME_OF_FLIGHT_DISTANCE 50
-#define MIN_IR_VALUE 70
-#define THRESHOLD_IR_DETECTS_WALL 100
-#define NORMAL_SETTING 0,0
-#define MAX_FINISH_TOF_DIST 180
-#define MIN_FINISH_TOF_DIST 120
+
 
 /**/
 
@@ -41,9 +23,7 @@ static THD_FUNCTION(Navigation, arg) {
 	chRegSetThreadName(__FUNCTION__);
 	(void)arg;
 
-	systime_t time;
-
-//	uint8_t activation = ARRET, mode = ARRET;
+	//systime_t time;
 
 	// Pour initialisation plus fiable
 	chThdSleepMilliseconds(300);
